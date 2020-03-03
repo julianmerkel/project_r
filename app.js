bdde233f3d101be642292a052fa4db4cd108b83b
mulc <buermarc2@gmail.com>
2020-03-03 18:53:55 +0100

    Hello World with send and app

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.send('hallo');
 
 });
 
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!');
+app.post('/bill', (req, res) => {
+    var bill_id=req.body.billId;
+    var bill_price=req.body.billPrice;
+    console.log("id: " + bill_id + ", price:  " +bill_price);
+    res.end("yes");
 });
+
+app.listen(8080, () => {
+    console.log("Started on PORT 8080");
+})
+
+
