fcf3889d8eca9e5b5dbfa0a3eea1c46d8d0f8162
mulc <buermarc2@gmail.com>
2020-03-06 13:48:54 +0100

    Dummy Vat ID check

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var bodyParser = require('body-parser');
 var app = express();
 var documentGenerator = require('./documentGenerator');
 
+function vatIdIsValid(vatId) {
+    return true;
+}
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
@@ -25,6 +28,7 @@ app.post('/bill', (req, res) => {
     var umsatzSteuerNummer=req.body.umsatzSteuerNummer
     var geldBetrag=req.body.geldBetrag;
     var beschreibung=req.body.beschreibung;
+    generateDocunment(req.body);
     console.log("id: " + bill_id + ", price:  " +bill_price);
     res.end("yes");
 });
@@ -33,7 +37,17 @@ app.listen(8080, () => {
     console.log("Started on PORT 8080");
 })
 
+
+function checkForVATID(jsondata) {
+   if (vatIdIsValid(jsondata.umsatzSteuerNummer)) {
+        json.mwsteuerBetrag = geldBetrag * 0.19;
+   } else { 
+       json.mwsteuerBetrag = 0;
+   }
+}
+
 function generateDocunment(data) {
+    checkForVATID(data);
     documentGenerator.generate(data);
 }
 
