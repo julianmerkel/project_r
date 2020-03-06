2d78cee83c13ceacbcb386a405e0d8bf2632a382
Merkel <merkel.julian@web.de>
2020-03-06 11:16:52 +0100

    hallo

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ app.get('/', function (req, res) {
 
 });
 
+
+app.post('/postSth', function (req, res) {
+
+    var x = req.body;
+
+});
+
 app.listen(8080, function () {
     console.log('Example app listening on port 8080!');
 });
+
+
